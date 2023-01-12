622bc9d3fd46a1ff59065bff2a4a638adfaa4b5a
Igor Guilherme Almeida Rocha <igorguilhermedev@gmail.com>
2023-01-12 19:33:29 -0300

    simpl

--- app/src/main/java/com/igor/caloriescalculator/adapters/MealSpinnerAdapter.java
+++ app/src/main/java/com/igor/caloriescalculator/adapters/MealSpinnerAdapter.java
@@ -1,29 +1,55 @@
 package com.igor.caloriescalculator.adapters;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.igor.caloriescalculator.R;
+import com.igor.caloriescalculator.model.MealClassification;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class MealSpinnerAdapter extends BaseAdapter {
 
+    private Context context;
+    private List<MealClassification> classifications;
 
+    public MealSpinnerAdapter(Context context){
+        this.context = context;
+        classifications = new ArrayList<>(Arrays.asList(MealClassification.values()));
+
+    }
     @Override
     public int getCount() {
-        return 0;
+        return classifications.size();
     }
 
     @Override
     public Object getItem(int position) {
-        return null;
+
+        return position;
     }
 
     @Override
     public long getItemId(int position) {
-        return 0;
+
+        return position;
     }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        return null;
+
+        View rootView = LayoutInflater.from(this.context).inflate(R.layout.spinner_item, parent, false);
+        TextView tvItemName = rootView.findViewById(R.id.tv_item_name);
+        ImageView ivImgItem = rootView.findViewById(R.id.iv_img_item);
+        tvItemName.setText(this.classifications.get(position).getName());
+        ivImgItem.setImageResource(R.drawable.clock_icon);
+        return rootView;
     }
 }
--- app/src/main/java/com/igor/caloriescalculator/model/MealClassification.java
+++ app/src/main/java/com/igor/caloriescalculator/model/MealClassification.java
@@ -1,4 +1,24 @@
 package com.igor.caloriescalculator.model;
 
 public enum MealClassification {
+    BREAKFAST("Café da manhã"),
+    AFTER_BREAKFAST("Desjejum"),
+    LUNCH("Almoço"),
+    AFTERNOON_BREAKFAST("Café da tarde"),
+    HAPPY_HOUR("Festa"),
+    DINNER("Jantar"),
+    DESSERT("Sobremesa"),
+    OTHER("Outro");
+
+    private String name;
+
+    private MealClassification(String name){
+        this.name = name;
+
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
 }
