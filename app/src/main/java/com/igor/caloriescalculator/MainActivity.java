13762566fce6db040d1c40f52b9d819d5ed67938
Igor Guilherme Almeida Rocha <igorguilhermedev@gmail.com>
2023-01-12 12:35:25 -0300

    add date picker and date picker image

--- app/src/main/java/com/igor/caloriescalculator/MainActivity.java
+++ app/src/main/java/com/igor/caloriescalculator/MainActivity.java
@@ -3,12 +3,20 @@ package com.igor.caloriescalculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView tvCaloriesLimit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initializeUiComponents();
+    }
+
+    private void initializeUiComponents(){
+        tvCaloriesLimit = findViewById(R.id.tv_calories_limit);
     }
 }
