69fc375bb5cf0ad8eaae5e5d68b2986bb4fb39de
Igor Guilherme Almeida Rocha <igorguilhermedev@gmail.com>
2023-01-12 19:46:46 -0300

    add more icons

--- app/src/main/java/com/igor/caloriescalculator/model/MealClassification.java
+++ app/src/main/java/com/igor/caloriescalculator/model/MealClassification.java
@@ -1,18 +1,21 @@
 package com.igor.caloriescalculator.model;
 
+import com.igor.caloriescalculator.R;
+
 public enum MealClassification {
-    BREAKFAST("Café da manhã"),
-    AFTER_BREAKFAST("Desjejum"),
-    LUNCH("Almoço"),
-    AFTERNOON_BREAKFAST("Café da tarde"),
-    HAPPY_HOUR("Festa"),
-    DINNER("Jantar"),
-    DESSERT("Sobremesa"),
-    OTHER("Outro");
+    BREAKFAST("Café da manhã", R.drawable.breakfast_icon),
+    AFTER_BREAKFAST("Desjejum", R.drawable.biscoit_icon),
+    LUNCH("Almoço", R.drawable.lunch_icon),
+    AFTERNOON_BREAKFAST("Café da tarde", R.drawable.breakfast_icon),
+    HAPPY_HOUR("Festa", R.drawable.happy_hour_icon),
+    DINNER("Jantar", R.drawable.dinner_icon),
+    DESSERT("Sobremesa", R.drawable.dessert_icon),
+    OTHER("Outro", R.drawable.pizza_icon);
 
     private String name;
+    private int imageId;
 
-    private MealClassification(String name){
+    private MealClassification(String name, int imageId){
         this.name = name;
 
     }
