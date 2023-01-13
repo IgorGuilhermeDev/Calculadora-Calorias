cb6a04813665d4a8d5a76fba98aedfff7b462d6f
Igor Guilherme Almeida Rocha <igorguilhermedev@gmail.com>
2023-01-13 12:21:51 -0300

    new package entities

---
+++ app/src/main/java/com/igor/caloriescalculator/model/entities/Meal.java
@@ -0,0 +1,81 @@
+package com.igor.caloriescalculator.model.entities;
+
+import com.igor.caloriescalculator.model.enums.MealClassification;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+public class Meal {
+
+    private Integer id;
+    private String name;
+    private LocalDateTime injestionDate;
+    private MealClassification mealClassificaion;
+    private Double foodCalories;
+
+    public Meal(){
+
+    }
+
+    public Meal(Integer id,String name, LocalDateTime injestionDate, MealClassification mealClassificaion, Double foodCalories) {
+        this.id = id;
+        this.name = name;
+        this.injestionDate = injestionDate;
+        this.mealClassificaion = mealClassificaion;
+        this.foodCalories = foodCalories;
+    }
+
+    public Integer getId(){
+        return this.id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public LocalDateTime getInjestionDate() {
+        return injestionDate;
+    }
+
+    public MealClassification getMealClassificaion() {
+        return mealClassificaion;
+    }
+
+    public Double getFoodCalories() {
+        return foodCalories;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setInjestionDate(LocalDateTime injestionDate) {
+        this.injestionDate = injestionDate;
+    }
+
+    public void setMealClassificaion(MealClassification mealClassificaion) {
+        this.mealClassificaion = mealClassificaion;
+    }
+
+    public void setFoodCalories(Double foodCalories) {
+        this.foodCalories = foodCalories;
+    }
+
+    public void setId(Integer id){
+        this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Meal meal = (Meal) o;
+        return Objects.equals(id, meal.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+}
