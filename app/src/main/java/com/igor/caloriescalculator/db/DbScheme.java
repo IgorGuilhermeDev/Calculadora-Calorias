c51f50df0680da3682cf974a2a01359707a2c619
Igor Guilherme Almeida Rocha <igorguilhermedev@gmail.com>
2023-01-13 12:21:29 -0300

    new package db

---
+++ app/src/main/java/com/igor/caloriescalculator/db/DbScheme.java
@@ -0,0 +1,12 @@
+package com.igor.caloriescalculator.db;
+
+public class DbScheme {
+
+    public static final String DB_NAME = "";
+    public static final int VERSION = 1;
+    public static final String SQL_CREATE_TABLES = "";
+
+    public static class Meal {
+
+    }
+}
